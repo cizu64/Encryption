3ac57fc28eac53f2d29614ad6155608f024c499c
cizu64 <cizu64@gmail.com>
2024-06-14 16:09:54 +0100

    AES Encryption

---
+++ Encryption/AESCryptography.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryption;
+public class AESCryptography
+{
+    public static string Encrypt(string plainText, string key)
+    {
+
+        using Aes aesAlg = Aes.Create();
+        aesAlg.Key = GetKey(key);
+        aesAlg.GenerateIV(); // Generating a random IV
+        aesAlg.Mode = CipherMode.CBC;
+        aesAlg.Padding = PaddingMode.PKCS7;
+
+        ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+        using MemoryStream msEncrypt = new MemoryStream();
+        msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+        {
+            swEncrypt.Write(plainText);
+        }
+
+        return Convert.ToBase64String(msEncrypt.ToArray());
+
+    }
+
+    public static string Decrypt(string cipherText, string key)
+    {
+
+        byte[] fullCipher = Convert.FromBase64String(cipherText);
+
+        using Aes aesAlg = Aes.Create();
+        aesAlg.Key = GetKey(key);
+        byte[] iv = new byte[aesAlg.BlockSize / 8];
+        byte[] cipherBytes = new byte[fullCipher.Length - iv.Length];
+
+        Array.Copy(fullCipher, iv, iv.Length);
+        Array.Copy(fullCipher, iv.Length, cipherBytes, 0, cipherBytes.Length);
+
+        aesAlg.IV = iv;
+        aesAlg.Mode = CipherMode.CBC;
+        aesAlg.Padding = PaddingMode.PKCS7;
+
+        ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+        using (MemoryStream msDecrypt = new MemoryStream(cipherBytes))
+        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+        {
+            return srDecrypt.ReadToEnd();
+        }
+    }
+
+    private static byte[] GetKey(string myKey)
+    {
+        using SHA1 sha = SHA1.Create();
+        // Convert the input string to a byte array and compute the hash
+        byte[] keyBytes = Encoding.UTF8.GetBytes(myKey);
+        byte[] hashBytes = sha.ComputeHash(keyBytes);
+
+        // Copy the first 16 bytes of the hash
+        byte[] key = new byte[16];
+        Array.Copy(hashBytes, key, key.Length);
+
+        return key;
+    }
+}
--- Encryption/Program.cs
+++ Encryption/Program.cs
@@ -1,3 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text.Json;
+using Encryption;
+
+
+string key = "251EFF08456499042EC695C0B20D4FE2BF0F1B03D71608E7CF1CF1DCFE99D212";
+
+var body = new
+{
+	amount = 200,
+	cardexpirydate = "2605",
+	cvv = "707",
+	datetime = "240218003431",
+	merchantid = "2058LA000000001",
+	merchantname = "BelemaPay Limited LA LANG",
+	otp = "633178",
+	pan = "5399839226202454",
+	terminalid = "20580001",
+	transactionid = "457864436867"
+};
+
+string serialized = JsonSerializer.Serialize(body);
+
+Console.WriteLine("----------------------------------------------");
+
+Console.WriteLine("Serialized data" + serialized);
+
+Console.WriteLine("----------------------------------------------\n");
+
+
+string result = AESCryptography.Encrypt(serialized, key);
+
+Console.WriteLine("----------------------------------------------");
+
+Console.WriteLine($"Encrypted result\n{result}");
+Console.WriteLine("----------------------------------------------\n");
+
+
+Console.WriteLine("----------------------------------------------");
+
+Console.WriteLine($"Decrypted result");
+
+string decrypotedResult = AESCryptography.Decrypt(result, key);
+
+Console.WriteLine(decrypotedResult);
+
+Console.WriteLine("----------------------------------------------\n");
 
